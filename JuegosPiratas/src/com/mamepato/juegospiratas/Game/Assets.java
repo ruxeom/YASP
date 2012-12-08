ec02f60c4c39eaac284f3119d0fdd8fc8241a387
ruxeom <fofojr2003@gmail.com>
2012-12-07 23:46:12 -0800

    Oops

--- JuegosPiratas/src/com/mamepato/juegospiratas/Game/Assets.java
+++ JuegosPiratas/src/com/mamepato/juegospiratas/Game/Assets.java
@@ -1,6 +1,12 @@
 package com.mamepato.juegospiratas.Game;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.mamepato.juegospiratas.framework.Pixmap;
+import com.mamepato.juegospiratas.framework.Sound;
+import com.mamepato.juegospiratas.framework.Graphics.PixmapFormat;
+import com.mamepato.juegospiratas.framework.implementation.AndroidPixmap;
 
 public class Assets {
 	public static Pixmap backGround;
@@ -9,8 +15,9 @@ public class Assets {
 	public static Pixmap orangeAndy;
 	public static Pixmap pinkAndy;
 	public static Pixmap yellowAndy;
+	public static Sound breakAndroidSound;
 	
 	public Assets() {
-		
+		backGround = new AndroidPixmap(BitmapFactory.decodeResource(getResources(), id), PixmapFormat.RGB565);
 	}
 }
